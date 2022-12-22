09aaa9fbbf9ac84a6dd1e16884992f578ad5096c
Izaù santos <nayahto.147@gmail.com>
2022-12-22 08:34:52 -0300

    main and divs
    a creation of a main content div inside of him

---
+++ projeto_react_01/src/components/headerNMain/Head.jsx
@@ -0,0 +1,11 @@
+import "./Head.css"
+
+function Head() {
+    return(
+<>
+    <h1>Nayahto</h1>
+</>
+    )
+}
+
+export default Head;
---
+++ projeto_react_01/src/components/headerNMain/Main.jsx
@@ -0,0 +1,24 @@
+import './Main.css'
+
+function Main() {
+return(
+<>
+    <div id='generalBoxes'>
+        <div className="mainBox">
+
+
+            <div>1</div>
+            <div>1</div>
+            <div>1</div>
+            <div>1</div>
+            <div>1</div>
+            <div>1</div>
+            <div>1</div>
+        </div>
+        <div className="rightBox"></div>
+    </div>
+</>
+)    
+}
+
+export default Main
